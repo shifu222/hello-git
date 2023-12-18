2977919d2d88ddd079ffd31715181dc50e64ac59
Shifu <gabrielcastillejo25@gmail.com>
2023-12-18 00:06:11 -0500

    agrego array de nombres

--- hola.java
+++ hola.java
@@ -4,5 +4,11 @@ class hola{
         for(int i:numero){
             System.out.print(i+" ");
         }
+        System.out.print("\n");
+        String[] nombres = {"Gabriel","Estefano","Julian"};
+        for(String nombre:nombres){
+            System.out.print(nombre +" ");
+        }
+
     }
 }
